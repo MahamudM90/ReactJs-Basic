d8328a206a250b9da7ca842d037ab87d10d2e8e9
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-05-26 15:46:01 +0600

    count

---
+++ Countdown state.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react';
+
+import './App.css';
+
+
+function App() {
+  
+
+ 
+  return (
+    <div className="App">
+      <div className="App-header">
+     
+
+  <MovieCounter></MovieCounter>
+   
+     </div>
+     </div>
+  );
+}
+ function MovieCounter () {
+   const [count, setCount] = useState(0);
+  
+   return (
+    <div>
+      <p>Clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
