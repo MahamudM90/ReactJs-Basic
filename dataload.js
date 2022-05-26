5f7d6340d867a8e6abe433800a60c3bbaa996528
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-05-26 14:56:49 +0600

    first commit

---
+++ dataload.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+
+function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => {setProducts(data);
+      console.log(data);
+      
+       } )
+      .catch(error => console.log(error))
+  
+  }, [])
+
+
+ 
+  return (
+    <div className="App">
+      <div className="App-header">
+     
+<h1> product Loaded:{products.length}</h1>
+<ul> 
+ {
+  products.map(product=> <li>{product.id}</li>)
+  }
+</ul>
+  
+   
+     </div>
+     </div>
+  );
+}
+
+
+
+
+
+export default App;
